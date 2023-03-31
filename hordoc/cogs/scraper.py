088f0501fa52d91d672da7721a8143d994873baf
Mischa Untaga <99098079+MischaU8@users.noreply.github.com>
2023-03-31 20:24:34 +0200

    incremental scraping

--- hordoc/cogs/scraper.py
+++ hordoc/cogs/scraper.py
@@ -1,14 +1,15 @@
 from collections import Counter
 from dataclasses import asdict
 from pprint import pprint as pp
+from typing import List, Optional
 
 import discord
 from discord.ext import commands
 from discord.ext.commands import Context
 
+import sqlite_utils
 from sqlite_utils.db import Table
 
-from hordoc.bot import HorDocBot
 from hordoc.data import (
     GuildItem,
     ForumChannelItem,
@@ -19,62 +20,104 @@ from hordoc.data import (
 
 
 class Scraper(commands.Cog):
-    """
-    Syncs the command tree.
-    From https://gist.github.com/AbstractUmbra/a9c188797ae194e592efe05fa129c57f
-    """
-
-    def __init__(self, bot: HorDocBot):
+    def __init__(
+        self,
+        bot: commands.Bot,
+        db: sqlite_utils.Database,
+        guild_ids: List[int],
+        forum_ids: List[int],
+    ):
         self.bot = bot
+        self.db = db
+        self.guild_ids = guild_ids
+        self.forum_ids = forum_ids
+        self.stats: Counter = Counter()
+        self.seen_authors: set[int] = set()
+
+    async def scrape_thread(
+        self, thread: discord.Thread, after_msg_id: Optional[int] = None
+    ) -> Optional[int]:
+        max_msg_id = after_msg_id
+        t = ThreadItem.from_discord(thread)
+        pp(t)
+        Table(self.db, "threads").insert(asdict(t), pk="id", replace=True)
+        if after_msg_id:
+            # start from beginning, do incremental based on last known.
+            after = discord.Object(after_msg_id)
+        else:
+            after = None
+        async for message in thread.history(after=after, limit=None):
+            if message.author.id not in self.seen_authors:
+                print(f"Got a new author: {message.author}")
+                self.seen_authors.add(message.author.id)
+                a = AuthorItem.from_discord(message.author)
+                pp(a)
+                Table(self.db, "authors").insert(asdict(a), pk="id", replace=True)
+                self.stats["authors"] += 1
+
+            m = MessageItem.from_discord(message)
+            pp(m)
+            Table(self.db, "messages").insert(asdict(m), pk="id", replace=True)
+            self.stats["messages"] += 1
+            if max_msg_id is None or m.id > max_msg_id:
+                max_msg_id = m.id
+        return max_msg_id
+
+    async def scrape_forum(self, forum: discord.ForumChannel) -> None:
+        f = ForumChannelItem.from_discord(forum)
+        pp(f)
+        Table(self.db, "channels").insert(asdict(f), pk="id", replace=True)
+        self.stats["forums"] += 1
+
+        known_threads = {}
+        for row in self.db.query(
+            """
+        select t.id as thread_id, max(m.id) as max_msg_id
+        from threads t join messages m on t.id = m.channel_id
+        where parent_id = :parent_id group by t.id
+            """,
+            {"parent_id": f.id},
+        ):
+            known_threads[row["thread_id"]] = row["max_msg_id"]
+
+        async def potential_scrape_thread(
+            thread: discord.Thread, stats_type: str
+        ) -> None:
+            if thread.id not in known_threads:
+                print(f"Scraping new {stats_type} {thread.id}")
+                max_msg_id = await self.scrape_thread(thread)
+                self.stats["new_" + stats_type] += 1
+                known_threads[thread.id] = max_msg_id
+            elif thread.last_message_id > known_threads[thread.id]:
+                print(
+                    f"Scraping known {stats_type} {thread.id} with new messages {thread.last_message_id}"
+                )
+                max_msg_id = await self.scrape_thread(thread, known_threads[thread.id])
+                self.stats["updated_" + stats_type] += 1
+                known_threads[thread.id] = max_msg_id
+            else:
+                print(f"Skipping known {stats_type} {thread.id}")
+                self.stats["skipped_" + stats_type] += 1
+
+        for thread in forum.threads:
+            await potential_scrape_thread(thread, "threads")
+        async for thread in forum.archived_threads(limit=None):
+            await potential_scrape_thread(thread, "archived_threads")
 
     async def scrape_guilds(self) -> Counter:
-        stats: Counter = Counter()
-        seen_authors = set()
+        self.stats.clear()
 
-        async def scrape_thread(thread: discord.Thread) -> None:
-            t = ThreadItem.from_discord(thread)
-            pp(t)
-            Table(self.bot.db, "threads").insert(asdict(t), pk="id", replace=True)
-            # TODO start from beginning, do incremental based on last known.
-            async for message in thread.history():
-                if message.author.id not in seen_authors:
-                    print(f"Got a new author: {message.author}")
-                    seen_authors.add(message.author.id)
-                    a = AuthorItem.from_discord(message.author)
-                    pp(a)
-                    Table(self.bot.db, "authors").insert(
-                        asdict(a), pk="id", replace=True
-                    )
-                    stats["authors"] += 1
-
-                m = MessageItem.from_discord(message)
-                pp(m)
-                Table(self.bot.db, "messages").insert(asdict(m), pk="id", replace=True)
-                stats["messages"] += 1
-
-        guilds = [guild for guild in self.bot.guilds if guild.id in self.bot.guilds_ids]
+        guilds = [guild for guild in self.bot.guilds if guild.id in self.guild_ids]
         for guild in guilds:
             g = GuildItem.from_discord(guild)
             pp(g)
-            Table(self.bot.db, "guilds").insert(asdict(g), pk="id", replace=True)
-            stats["guilds"] += 1
+            Table(self.db, "guilds").insert(asdict(g), pk="id", replace=True)
+            self.stats["guilds"] += 1
 
-            forums = [
-                channel for channel in guild.forums if channel.id in self.bot.forums_ids
-            ]
-            for forum in forums:
-                f = ForumChannelItem.from_discord(forum)
-                pp(f)
-                Table(self.bot.db, "channels").insert(asdict(f), pk="id", replace=True)
-                stats["forums"] += 1
-                for thread in forum.threads:
-                    await scrape_thread(thread)
-                    stats["threads"] += 1
-
-                async for thread in forum.archived_threads(limit=None):
-                    await scrape_thread(thread)
-                    stats["archived_threads"] += 1
-        return stats
+            for forum in guild.forums:
+                if forum.id in self.forum_ids:
+                    await self.scrape_forum(forum)
+        return self.stats
 
     @commands.command()
     @commands.guild_only()
