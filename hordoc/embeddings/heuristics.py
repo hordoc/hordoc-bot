1d16e3ae231ccce89df9bed25cd2f3a94602da1b
Ki <k.allegrain@gmail.com>
2023-03-31 19:47:28 +0200

    implemented heuristics

--- hordoc/embeddings/heuristics.py
+++ hordoc/embeddings/heuristics.py
@@ -1,14 +1,22 @@
+from math import floor
 import sqlite_utils
 from sqlite_utils import Database
+from sentence_transformers import SentenceTransformer, util
+
+from hordoc.embeddings.embeddings_search import find_most_similar, get_embeddings
 # Connect to the database file
 db : Database = sqlite_utils.Database('../../data/horde_help.db')
-print(db.table_names())
 
+def print_and_buffer(txt, buffer):
+	print(txt)
+	return buffer + txt + '\n'
 
+time_delta_milliseconds = 1000 * 60 * 60 # 1 hour
+score_threshold = 0.8
 def get_messages_with_thanks_and_reply (db: Database):
 	excluded_users = ('db0','Airic')
 	query = f"""
-	select chan_id, msg_id, uid, t, user, msg, r, te, re
+	select chan_id, msg_id, uid, t, user, msg, r
 	from horde_help
 	where "msg" like "%thanks%"
 	and ("r" is not null and "r" != "")
@@ -22,9 +30,12 @@ def get_message_by_id (db: Database, msg_id):
 	from horde_help
 	where msg_id = {msg_id}
 	order by msg_id"""
-	return db.execute_returning_dicts(query)[0]
+	result= db.execute_returning_dicts(query)
+	if(len(result) == 0):
+		return None
+	return result[0]
 
-def get_messages_by_time_delta (db: Database, start_time, delta):
+def get_messages_by_time_delta (db: Database, start_time, delta, channel_id):
 	query = f"""
 	select chan_id, msg_id, uid, t, user, msg, r, te, re
 	from horde_help
@@ -33,13 +44,43 @@ def get_messages_by_time_delta (db: Database, start_time, delta):
 	order by msg_id"""
 	return db.execute_returning_dicts(query)
 
-def get_messages_by_time_delta_and_uid (db: Database, start_time, delta, uid):
+def get_messages_by_time_delta_and_uid (db: Database, start_time, delta, uid, channel_id):
 	query = f"""
-	select chan_id, msg_id, uid, t, user, msg, r, te, re
+	select chan_id, msg_id, uid, t, user, msg
 	from horde_help
-	where t <= {start_time}
-	and t > {start_time - delta}
+	where t < {start_time}
+	and t >= {start_time - delta}
 	and uid = {uid}
+	and chan_id = {channel_id}
 	order by msg_id"""
 	return db.execute_returning_dicts(query)
 
+
+if __name__ == '__main__' :
+	messages = get_messages_with_thanks_and_reply(db)
+	txt = ''
+	for message in messages:
+		reply = get_message_by_id(db, message['r'])
+		if reply is None:
+			continue
+		if(reply['uid'] == message['uid']):
+			continue
+		asker_messages = get_messages_by_time_delta_and_uid(db, int(message['t']), time_delta_milliseconds, message['uid'], message['chan_id'])
+		if(len(asker_messages) == 0):
+			continue
+		reply_embeddings = get_embeddings([reply['msg']])[0]
+		askers_messages_text = [m['msg'] for m in asker_messages]
+		askers_messages_embeddings = get_embeddings(askers_messages_text)
+		most_similars =  util.semantic_search(reply_embeddings, askers_messages_embeddings, 5)[0]
+		most_similars = list(filter(lambda m: m['score'] > score_threshold, most_similars))
+		if(len(most_similars) == 0):
+			continue
+		most_similar_messages = [{'msg' : asker_messages[m['corpus_id']] , 'score' : floor( m['score'] * 100)}  for m in most_similars]
+		txt = print_and_buffer('Thanks message : ' + message['msg'],txt)
+		txt = print_and_buffer( 'Reply : ' + reply['msg'],txt)
+		most_similar_messages_text = [m['msg']['msg'] +' '+ str(m['score']) + '%' for m in most_similar_messages]
+		txt = print_and_buffer('Found possible questions :' +  str(most_similar_messages_text),txt)
+		txt = print_and_buffer('-----------------',txt)
+		#print(asker_messages)
+	with open('output.txt', 'w+',  encoding="utf-16") as f:
+		f.write(txt)
