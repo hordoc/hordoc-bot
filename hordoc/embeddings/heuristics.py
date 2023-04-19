861e0e2f5ac97e7e09b6826df72c5a22e30dcf11
Mischa Untaga <99098079+MischaU8@users.noreply.github.com>
2023-04-19 14:59:32 +0200

    extract method

--- hordoc/embeddings/heuristics.py
+++ hordoc/embeddings/heuristics.py
@@ -12,8 +12,8 @@ from sentence_transformers import SentenceTransformer, util
 db: Database = sqlite_utils.Database("../../data/horde_help.db")
 
 # EMBEDDINGS_MODEL = "sentence-transformers/gtr-t5-large"
-EMBEDDINGS_MODEL = "intfloat/e5-large"
-# EMBEDDINGS_MODEL = "intfloat/e5-small"
+# EMBEDDINGS_MODEL = "intfloat/e5-large"
+EMBEDDINGS_MODEL = "intfloat/e5-small"
 
 model = SentenceTransformer(EMBEDDINGS_MODEL)
 
@@ -56,16 +56,6 @@ def get_message_by_id(db: Database, msg_id):
     return result[0]
 
 
-def get_messages_by_time_delta(db: Database, start_time, delta, channel_id):
-    query = f"""
-    select chan_id, msg_id, uid, t, user, msg, r, te, re
-    from horde_help
-    where t <= {start_time}
-    and t > {start_time - delta}
-    order by msg_id"""
-    return db.execute_returning_dicts(query)
-
-
 def get_messages_by_time_delta_and_uid(
     db: Database, start_time, delta, uid, channel_id
 ):
@@ -83,7 +73,7 @@ def get_messages_by_time_delta_and_uid(
 
 def load_annotations(db: Database):
     annotations = {}
-    counters = Counter()
+    counters: Counter[str] = Counter()
 
     for row in db.query(
         """
@@ -101,77 +91,89 @@ def load_annotations(db: Database):
     return annotations, counters
 
 
-def main():
+def main():  # noqa: C901
     annotations, annotation_counters = load_annotations(db)
-    counters = Counter()
+    counters: Counter[str] = Counter()
+
+    def process_message(message):
+        if message["msg_id"] not in annotations:
+            counters["ignore_no_annotation"] += 1
+            return
+        annotation = annotations[message["msg_id"]]
+        counters["messages_in"] += 1
+        reply = get_message_by_id(db, message["r"])
+        if reply is None:
+            counters["skip_reply_not_found"] += 1
+            counters["messages_skipped"] += 1
+            return
+        if reply["uid"] == message["uid"]:
+            counters["skip_reply_to_self"] += 1
+            counters["messages_skipped"] += 1
+            return
+        if reply["msg_id"] != annotation["reply_msg_id"]:
+            counters["bad_reply_id"] += 1
+            counters["messages_skipped"] += 1
+            return
+        asker_messages = get_messages_by_time_delta_and_uid(
+            db,
+            int(message["t"]),
+            time_delta_milliseconds,
+            message["uid"],
+            message["chan_id"],
+        )
+        if len(asker_messages) == 0:
+            counters["skip_no_asker_messages"] += 1
+            counters["messages_skipped"] += 1
+            return
+        reply_embeddings = get_embeddings([reply["msg"]])[0]
+        askers_messages_text = [m["msg"] for m in asker_messages]
+        askers_messages_embeddings = get_embeddings(askers_messages_text)
+        most_similars = util.semantic_search(
+            reply_embeddings, askers_messages_embeddings, 5
+        )[0]
+        most_similars = list(
+            filter(lambda m: m["score"] >= score_threshold, most_similars)
+        )
+        if len(most_similars) == 0:
+            counters["skip_no_most_similars"] += 1
+            counters["messages_skipped"] += 1
+            return
+        most_similar_messages = [
+            {"msg": asker_messages[m["corpus_id"]], "score": floor(m["score"] * 100)}
+            for m in most_similars
+        ]
+
+        if annotation["label"] == "OK":
+            counters["accurate_out"] += 1
+            if (
+                most_similar_messages[0]["msg"]["msg_id"]
+                == annotation["question_msg_id"]
+            ):
+                counters["accurate_question"] += 1
+
+        txt = (
+            "LINK  : "
+            f"https://discord.com/channels/{guild_id}/{message['chan_id']}/{message['msg_id']}\n"
+        )
+        q_bucket = score_bucket(most_similar_messages[0]["score"])
+        counters[f"score_q{q_bucket}+"] += 1
+        for m in most_similar_messages:
+            txt += f"Q?{m['score']:3d}%  {m['msg']['msg']}\n"
+        txt += f"REPLY : {reply['msg']}\n"
+        txt += f"THANKS: {message['msg']}\n"
+        txt += "-----------------\n"
+        # print(asker_messages)
+        counters["messages_out"] += 1
+        return txt
+
     start = time.time()
     messages = get_messages_with_thanks_and_reply(db)
     with open("output.txt", "w", encoding="utf-8") as f:
         for message in messages:
-            txt = ""
-            if message["msg_id"] not in annotations:
-                counters["ignore_no_annotation"] += 1
-                continue
-            annotation = annotations[message["msg_id"]]
-            counters["messages_in"] += 1
-            reply = get_message_by_id(db, message["r"])
-            if reply is None:
-                counters["skip_reply_not_found"] += 1
-                continue
-            if reply["uid"] == message["uid"]:
-                counters["skip_reply_to_self"] += 1
-                continue
-            if reply["msg_id"] != annotation["reply_msg_id"]:
-                counters["bad_reply_id"] += 1
-                continue
-            asker_messages = get_messages_by_time_delta_and_uid(
-                db,
-                int(message["t"]),
-                time_delta_milliseconds,
-                message["uid"],
-                message["chan_id"],
-            )
-            if len(asker_messages) == 0:
-                counters["skip_no_asker_messages"] += 1
-                continue
-            reply_embeddings = get_embeddings([reply["msg"]])[0]
-            askers_messages_text = [m["msg"] for m in asker_messages]
-            askers_messages_embeddings = get_embeddings(askers_messages_text)
-            most_similars = util.semantic_search(
-                reply_embeddings, askers_messages_embeddings, 5
-            )[0]
-            most_similars = list(
-                filter(lambda m: m["score"] >= score_threshold, most_similars)
-            )
-            if len(most_similars) == 0:
-                counters["skip_no_most_similars"] += 1
-                continue
-            most_similar_messages = [
-                {"msg": asker_messages[m["corpus_id"]], "score": floor(m["score"] * 100)}
-                for m in most_similars
-            ]
-
-            if annotation["label"] == "OK":
-                counters["accurate_out"] += 1
-                if (
-                    most_similar_messages[0]["msg"]["msg_id"]
-                    == annotation["question_msg_id"]
-                ):
-                    counters["accurate_question"] += 1
-
-            txt += ("LINK  : "
-                    f"https://discord.com/channels/{guild_id}/{message['chan_id']}/{message['msg_id']}\n")
-            q_bucket = score_bucket(most_similar_messages[0]["score"])
-            counters[f"score_q{q_bucket}+"] += 1
-            for m in most_similar_messages:
-                txt += f"Q?{m['score']:3d}%  {m['msg']['msg']}\n"
-            txt += f"REPLY : {reply['msg']}\n"
-            txt += f"THANKS: {message['msg']}\n"
-            txt += "-----------------\n"
-            # print(asker_messages)
-            counters["messages_out"] += 1
-            print(txt)
-            f.write(txt)
+            txt = process_message(message)
+            if txt:
+                print(txt)
+                f.write(txt)
 
     end = time.time()
 
